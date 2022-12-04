7980329dbe3a3b31b7bf602a9f8c59c174a74a45
nisha <nishasinghrawat2022@gmail.com>
2022-12-04 12:19:27 +1100

    bootstrap

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.png';
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src={logo} className="App-logo img-fluid" alt="logo"></img>
+</header>
+    </div>
+  );
+}
+ export default App;
